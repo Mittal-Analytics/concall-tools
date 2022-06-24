c22fd57277017ca73ec977596b271f483cbfdf90
Pratyush Mittal <pratyushmittal@gmail.com>
2022-06-24 10:24:06 +0530

    Minor improvements

--- src/concall_tools.py
+++ src/concall_tools.py
@@ -1,13 +1,11 @@
 import fitz
-import enchant
-d = enchant.Dict("en_US")
 
 from speakers.extraction import Speaker
+from speakers.extraction import get_speakers_capitals as _get_speakers_capitals
 from speakers.extraction import (
     get_speakers_from_text as _get_speakers_from_text,
 )
 from speakers.extraction import get_speakers_in_bold as _get_speakers_in_bold
-from speakers.extraction import get_speakers_capitals as _get_speakers_capitals
 
 
 def get_speakers(pdf_name, algorithm="auto"):
@@ -28,20 +26,10 @@ def get_speakers(pdf_name, algorithm="auto"):
 
     doc = fitz.open(pdf_name)
     speakers = []
-    names=[]
-    speakers2=[]
     if algorithm == "auto" or algorithm == "bold":
         speakers = _get_speakers_in_bold(doc)
     if not speakers and (algorithm == "auto" or algorithm == "plain"):
         speakers = _get_speakers_from_text(doc)
-    speakers1=_get_speakers_capitals(pdf_name)
-    for s in speakers:
-        names.append(s[0])
-    #print(names)
-    for s in speakers1:
-        if s[0] in names:
-            speakers2.append(s)
-        if s[0] not in names:
-            if s[1]!=None:
-                speakers2.append(s)
-    return speakers2
+
+    speakers1 = _get_speakers_capitals(pdf_name)
+    return speakers1
